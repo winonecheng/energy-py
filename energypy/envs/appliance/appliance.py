3f7eff793ce2230552d059db6175d3edf3f05d3c
winonecheng <F74036328@mail.ncku.edu.tw>
2020-05-07 15:20:06 +0800

    Remove attr index_col which make error

--- energypy/envs/appliance/appliance.py
+++ energypy/envs/appliance/appliance.py
@@ -41,10 +41,10 @@ class Appliance(BaseEnv):
         )
 
         # load init power data
-        self.init_power_df = pd.read_csv(join(dataset, 'init_appliance.csv'), index_col=0, parse_dates=True, usecols=[name])
+        self.init_power_df = pd.read_csv(join(dataset, 'init_appliance.csv'), parse_dates=True, usecols=[name])
 
         # load tolerable power data
-        self.tolerable_power_df = pd.read_csv(join(dataset, 'tolerable.csv'), index_col=0, parse_dates=True, usecols=[name])
+        self.tolerable_power_df = pd.read_csv(join(dataset, 'tolerable.csv'), parse_dates=True, usecols=[name])
     
     def __repr__(self):
         return f'<energypy APPLIANCE env - {self.name}>'
