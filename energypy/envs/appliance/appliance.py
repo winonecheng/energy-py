17c156d32e77fced2318d71fcb884d0f398d2ba2
winonecheng <F74036328@mail.ncku.edu.tw>
2020-05-13 19:01:00 +0800

    Set action range [-0.4, 0]

--- energypy/envs/appliance/appliance.py
+++ energypy/envs/appliance/appliance.py
@@ -52,7 +52,7 @@ class Appliance(BaseEnv):
         return self.observation
 
     def _step(self, action):
-        _action = (action[0][0] - 1) * 0.1 # range [-1, 1] -> [-0.2, 0]
+        _action = (action[0][0] - 1) * 0.2 # range [-1, 1] -> [-0.4, 0]
         old_power = self.get_state_variable(self.name)
         _new_power = old_power + _action
         tolerable_power = self.tolerable_power_df.iloc[self.start + self.steps]
